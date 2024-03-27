857d3c024fc753630f4e1dec7bfecf654aacbfca
André Luis dos Santos <alsss.ads@gmail.com>
2024-03-26 21:34:08 -0300

    feat: :sparkles: Usa dados no json de exp

--- components/ExpWindow.vue
+++ components/ExpWindow.vue
@@ -1,26 +1,11 @@
 <template>
   <DialogWindow title="Experiências">
-    <ExpItem v-bind="exp" />
+    <ExpItem v-for="x in exp" v-bind="x" :key="x.title" />
   </DialogWindow>
 </template>
 
 <script setup lang="ts">
-const exp = {
-  title: 'Sisped',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n' +
-      '        Sed a ipsum consectetur, scelerisque nisi nec, dignissim nisi.\n' +
-      '        Proin tincidunt auctor arcu, fermentum suscipit leo porta at. Suspendisse pretium ante non.' ,
-  date: 'Fev/2018 - Atual',
-  contributions: ['Desenvolvimento de novas funcionalidades', 'Manutenção de funcionalidades existentes', 'Correção de bugs'],
-  technologies: ['Java', 'Spring Boot', 'Angular', 'PostgreSQL']
-}
-</script>
+import expDb from '~/db/exp.json'
 
-<style scoped>
-#container::before {
-  content: "";
-  float: left;
-  visibility: hidden;
-  height: calc(50px * 3);
-}
-</style>
+const exp = expDb.exp
+</script>
