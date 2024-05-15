66e0e4fc0aef366ee37e3bf1b0d48a475f72353f
André Luis dos Santos <alsss.ads@gmail.com>
2024-05-14 23:08:50 -0300

    fix: Corrige escrita

--- components/ProfileWindow.vue
+++ components/ProfileWindow.vue
@@ -13,7 +13,7 @@
         <div>
           <h2>Opa! eai?</h2>
           <p>Iniciei em programação quando entrei no curso de <strong>Analise e desenvolvimento de sistemas</strong> no IFBA - Irecê em 2018</p>
-          <p>De lá para cá eu estudei um pouco de tudo, mas me firmei no Front-end. Isso aconteceu quando demostrei interesse e preocupação com a experiencia do usuário, isso desdes meus primeiros algoritmos em C quando nem sabia o que era UI/UX. </p>
+          <p>De lá para cá eu estudei um pouco de tudo, mas me firmei no Front-end. Isso aconteceu quando demostrei interesse e preocupação com a experiencia do usuário, isso desde meus primeiros algoritmos em C quando nem sabia o que era UI/UX. </p>
           <p>Meu TCC foi sobre como aplicar Design Centrado no Usuário como forma de trazer assertividade nas decisões de software em um time ágil.</p>
           <p>Me dediquei a estudar <strong>Vue</strong> e hoje trabalho com <strong>Nuxt</strong>, atualmente estou estudando principalmente sobre design patterns, meu objetivo é conseguir escrever código de qualidade, que me ajude a resolver problemas de forma rápida e sustentável </p>
           <h3>Isso é só um resumo!</h3>
